7f2ca4acb2e30f274c208e2326524187c5bafe93
Omar <omar6688.oc@gmail.com>
2022-08-15 00:33:23 -0500

    Estructura de cliente

--- persona.py
+++ persona.py
@@ -1,3 +1,26 @@
 class Persona():
-    pass
+    name: str
+    cc: str
+    age: int
+    email:str
+    address: str
+    personas: dict = {}
+    def __init__(self, name:str, cc:str, age:int, email:str, address:str):
+        self.name   =   name
+        self.cc     =   cc
+        self.age    =   age
+        self.email  =   email
+        self.address=   address
+        Persona.personas[cc] = self
+    
+    @classmethod
+    def ToString(cls):
+        for persona in cls.personas.keys():
+            print(f"""
+Nombre: {persona.name}
+Edad: {persona.age}
+Direción: {persona.address}
+E-mail: {persona.email}
+                        """)
+
     
